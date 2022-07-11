5a531f71aa20d897ee8023ed3f124289f7510e71
Silvio <silviotassini@gmail.com>
2022-07-11 13:26:33 -0300

    create a search field

--- contatos/views.py
+++ contatos/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.paginator import Paginator
-
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 from .models import Contato
 
 def index(request):
@@ -19,3 +20,19 @@ def ver_contato(request, contato_id):
     if not contato.exibir:
         raise Http404()
     return render(request,'contatos/ver_contato.html',{'contato':contato})
+
+def  busca(request):
+    termo = request.GET.get('termo')
+    if termo is None:
+        raise Http404()
+    campos = Concat('nome',Value(" "), 'sobrenome') 
+    contatos = Contato.objects.annotate(
+        nome_completo = campos
+    ).filter( Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo) )
+    #contatos = Contato.objects.order_by('-id').filter( 
+    #        Q(nome__icontains=termo) | Q(sobrenome__icontains=termo)
+    #        , exibir=True )
+    paginator = Paginator(contatos, 10)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request,'contatos/busca.html',{'contatos':contatos})
