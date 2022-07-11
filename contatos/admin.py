ae5c271fc85c7d5eb781ffd29228a1b551a5fef1
Silvio <silviotassini@gmail.com>
2022-07-11 12:57:14 -0300

    Paginator and a simple filter

--- contatos/admin.py
+++ contatos/admin.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from .models import Categoria, Contato
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome','sobrenome', 'email')
+    list_display = ('id', 'nome','sobrenome', 'telefone', 'email','exibir')
     list_display_links = ('nome','sobrenome')
     list_filter = ('nome','sobrenome')
     search_fields = ('nome','sobrenome')
+    list_editable = ('telefone','exibir')
 
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
